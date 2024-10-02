16699cfb6bc1baa34e5f2fbde78ccede0760af33
Ngoc Trung <ngoctrung2k42203@gmail.com>
2024-10-02 23:19:37 +0700

    Done build mainPage

--- app/src/main/java/com/finalmobile/app_sell/MainActivity.java
+++ app/src/main/java/com/finalmobile/app_sell/MainActivity.java
@@ -3,17 +3,25 @@ package com.finalmobile.app_sell;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.Toolbar;
+import android.widget.ViewFlipper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
-    private ListView listView;
+    private ListView listViewmain;
+    private ViewFlipper viewFlipper;
+    private RecyclerView recyclerView;
+    private NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
     public void Anhxa()
     {
         toolbar.findViewById(R.id.toolbarmainpage);
-        listView.findViewById(R.id.lvmainpage);
+        listViewmain.findViewById(R.id.lvmainpage);
+        viewFlipper.findViewById(R.id.viewlipper);
+        recyclerView.findViewById(R.id.recyclerview);
+        navigationView.findViewById(R.id.naviview);
     }
 }
