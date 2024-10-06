b5605832c279502fc3e7b5245fcbe8f5f53f87ab
Ngoc Trung <ngoctrung2k42203@gmail.com>
2024-10-06 21:14:42 +0700

    ViewFliper QC

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
--- app/src/main/java/com/finalmobile/sellapp/MainActivity.java
+++ app/src/main/java/com/finalmobile/sellapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.finalmobile.sellapp;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toolbar;
 import android.widget.ViewFlipper;
@@ -8,11 +13,17 @@ import android.widget.ViewFlipper;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
@@ -20,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     NavigationView navigationView;
     ListView listView;
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +43,42 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+    private void ActionViewFlipper()
+    {
+        List<String> qc = new ArrayList<>();
+        qc.add("https://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-Le-hoi-phu-kien-800-300.png");
+        qc.add("https://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-HC-Tra-Gop-800-300.png");
+        qc.add("https://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-big-ky-nguyen-800-300.jpg");
+        for (int i = 0; i < qc.size(); i++)
+        {
+            ImageView imageView = new ImageView(getApplicationContext());
+            Glide.with(getApplicationContext()).load(qc.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+        }
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.setAutoStart(true);
+        Animation slide_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_right);
+        Animation slide_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out_right);
+        viewFlipper.setInAnimation(slide_in);
+        viewFlipper.setInAnimation(slide_out);
+    }
+    private void ActionBar()
+    {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+
     private void Anhxa()
     {
         toolbar.findViewById(R.id.toolbarMainpage);
@@ -38,5 +86,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.findViewById(R.id.recyclerview);
         navigationView.findViewById(R.id.navigationview);
         listView.findViewById(R.id.lvMainpage);
+        drawerLayout.findViewById(R.id.drawerlayout);
     }
 }
