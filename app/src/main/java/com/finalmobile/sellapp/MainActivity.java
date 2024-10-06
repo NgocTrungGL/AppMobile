9c68849f010efa012e4915d3f3d158c5abedb470
Ngoc Trung <ngoctrung2k42203@gmail.com>
2024-10-06 15:55:03 +0700

    fixed

--- app/src/main/java/com/finalmobile/sellapp/MainActivity.java
+++ app/src/main/java/com/finalmobile/sellapp/MainActivity.java
@@ -30,9 +30,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
     }
-
     private void Anhxa()
     {
         toolbar.findViewById(R.id.toolbarMainpage);
