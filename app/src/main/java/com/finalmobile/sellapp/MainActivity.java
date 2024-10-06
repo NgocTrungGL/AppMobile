4331d409b759578ef04c7fa76ab19a8a7cc2214a
Ngoc Trung <ngoctrung2k42203@gmail.com>
2024-10-06 15:26:14 +0700

    bulid page

--- app/src/main/java/com/finalmobile/sellapp/MainActivity.java
+++ app/src/main/java/com/finalmobile/sellapp/MainActivity.java
@@ -1,15 +1,25 @@
 package com.finalmobile.sellapp;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toolbar;
+import android.widget.ViewFlipper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerView;
+    NavigationView navigationView;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    }
+
+    private void Anhxa()
+    {
+        toolbar.findViewById(R.id.toolbarMainpage);
+        viewFlipper.findViewById(R.id.viewFlipper);
+        recyclerView.findViewById(R.id.recyclerview);
+        navigationView.findViewById(R.id.navigationview);
+        listView.findViewById(R.id.lvMainpage);
     }
 }
